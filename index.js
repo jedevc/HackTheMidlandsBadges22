4e826e5bfed0fe6d51047624494ab4325a290a52
Justin Chadwell <me@jedevc.com>
2022-06-17 15:00:22 +0100

    Add total budget for program

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ window.onload = () => {
   createBadgeModule().then(Module => {
     console.log('WebAssembly loaded!');
     var run_lua = Module.cwrap('run_lua', 'number', ['string']);
-    run_lua("print(1 + 2)")
+    run_lua("foo = foo + 1\nprint(foo)")
+    run_lua("foo = foo + 1\nprint(foo)")
+    run_lua("foo = foo + 1\nprint(foo)")
+    run_lua("foo = foo + 1\nprint(foo)")
+    run_lua("while true do\nend")
   });
 }
--- main.c
+++ main.c
@@ -1,30 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+
 #include "lua.h"
 #include <lauxlib.h>
 #include <lualib.h>
 
-#include <stdlib.h>
+#define BUDGET 100000
+#define BUDGET_STEPS (1000)
+
+lua_State* lua = NULL;
+int cost = 0;
 
 // TODO:
 //   - add global variables for title, content and canvas
-//   - preserve state from run-to-run (make sure to reset the counter)
 //   - parallelize using webworkers?
-//   - add to an html file
 //   - actually render as a badge!
 //   - snazzy transitions and animations
 
 void hook (lua_State *L, lua_Debug *ar) {
-	exit(0xf1);
+	cost += BUDGET_STEPS;
+	if (cost > BUDGET) {
+		exit(0xf1);
+	}
 }
 
 int run_lua(const char* script) {
-	lua_State* lua = luaL_newstate();
-	lua_pushnumber(lua, 100);
-	lua_setglobal(lua, "foo");
-	luaL_openlibs(lua);
+	cost = 0;
+	if (lua == NULL) {
+		lua = luaL_newstate();
+		lua_pushinteger(lua, 100);
+		lua_setglobal(lua, "foo");
+		luaL_openlibs(lua);
+		lua_sethook(lua, hook, LUA_MASKCOUNT, BUDGET_STEPS);
+	}
 	
-	lua_sethook(lua, hook, LUA_MASKCOUNT, 1000000);
-
 	int res = luaL_dostring(lua, script);
 
 	size_t len = 0;
@@ -32,7 +41,7 @@ int run_lua(const char* script) {
 
 	printf("%s\n", value);
 
-	lua_close(lua);
+	// lua_close(lua);
 
 	return 0;
 }
