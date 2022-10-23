d77c3c7c21d3b6ce3bd7e044ab00fda930832f6c
Justin Chadwell <me@jedevc.com>
2022-10-23 13:46:35 +0100

    Add smtp mail sending

--- backend/app/routers/onboarding.py
+++ backend/app/routers/onboarding.py
@@ -1,3 +1,9 @@
+import smtplib
+import ssl
+import os
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 
@@ -10,6 +16,12 @@ router = APIRouter()
 
 class Signup(schemas.UserCreate):
     badge: str
+
+
+SMTP_USERNAME = os.environ.get("SMTP_USERNAME")
+SMTP_PASSWORD = os.environ.get("SMTP_PASSWORD")
+SMTP_SERVER = os.environ.get("SMTP_SERVER")
+SMTP_PORT = int(os.environ.get("SMTP_PORT", 0))
 
 
 @router.post(
@@ -54,7 +66,40 @@ async def signup(
         store = crud.create_store(db, badge)
     store.data["token"] = tkn
 
+    if SMTP_SERVER:
+        mail = MIMEMultipart("alternative")
+        mail["Subject"] = onboarding_subject
+        mail["From"] = SMTP_USERNAME
+        mail["To"] = user.email
+        text = MIMEText(onboarding_template.format(user.name, user.email, tkn), "plain")
+        mail.attach(text)
+
+        ssl_context = ssl.create_default_context()
+        service = smtplib.SMTP_SSL(SMTP_SERVER, SMTP_PORT, context=ssl_context)
+        if SMTP_PASSWORD:
+            service.login(SMTP_USERNAME, SMTP_PASSWORD)
+        service.sendmail(SMTP_USERNAME, user.email, mail.as_string())
+        service.quit()
+    else:
+        # developer fallback!
+        print(f"emailing api token <{tkn}> to {user.email}")
+
     db.commit()
-    print(f"emailing api token <{tkn}> to {user.email}")
 
     return user
+
+
+onboarding_subject = "Welcome to the HackTheMidlands Badge Platform!"
+onboarding_template = """
+Hi {0}!
+
+You're receiving this email because someone signed up for the HackTheMidlands
+interactive badge service using your email address "{1}".
+
+Your API token for your badge is: "{2}"
+
+If you didn't request this email, then you ignore this safely.
+
+All the best,
+HTM
+"""
