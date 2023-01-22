4f136bcf60972a471e8eae72e50156da7c55d459
Justin Chadwell <me@jedevc.com>
2023-01-22 16:01:28 +0000

    Add a new home page

--- frontend/src/scripts/pages/home/index.js
+++ frontend/src/scripts/pages/home/index.js
@@ -8,81 +8,24 @@ import useLocalStorage from "../../../hooks/useLocalStorage";
 import { api } from "../../api";
 
 export const Home = ({}) => {
-  const location = useLocation();
-  const query = new URLSearchParams(location.search);
-  if (query.has("token")) {
-    return <Login token={query.get("token")} />;
-  } else {
-    return <Scanner />;
-  }
-};
-
-const Login = ({ token }) => {
-  const history = useHistory();
-  const [storedKey, setStoredKey] = useLocalStorage("token", undefined);
-
-  useEffect(() => {
-    api({ path: "permissions", token })
-      .then((permissions) => {
-        const id = permissions.store.badges.write[0];
-        if (!permissions.store.keys.read.includes("code")) {
-          throw new Error("Provided token cannot read code");
-        }
-        if (!permissions.store.keys.write.includes("code")) {
-          throw new Error("Provided token cannot write code");
-        }
-        setStoredKey(token);
-        return id;
-      })
-      .then((id) => {
-        history.push("/dev/" + id);
-      })
-      .catch((error) => {
-        history.push("/error", { error });
-      });
-  }, []);
-
-  return <></>;
-};
-
-const reToken = /^(.*?)([a-z]{3})([a-zA-Z0-9]+)$/;
-
-const Scanner = () => {
-  const [target, setTarget] = useState();
-
-  const handleScan = (result) => {
-    if (result) {
-      const match = result.match(reToken);
-      if (!match) return;
-      const [prefix, shortcode, tkn] = match.slice(1);
-      if (shortcode.toLowerCase() == "bdg") {
-        setTarget(shortcode + tkn);
-      }
-    }
-  };
-  const handleError = console.error;
-
   return (
     <div className={styles.pageContainer}>
       <div className={styles.page}>
         <div className={styles.title}>HackTheMidlands Badges</div>
         <div className={styles.contents}>
           <p>Welcome to the HackTheMidlands Badge Portal! 🎉</p>
-          <QrReader
-            style={{ width: "100%" }}
-            onScan={handleScan}
-            onError={handleError}
+          <Button
+            color="rgb(59, 102, 250)"
+            text="Try the demo now!"
+            link={`/demo`}
+            icon={<FaIdBadge />}
           />
-          {target ? (
-            <Button
-              color="rgb(59, 102, 250)"
-              text="View Badge"
-              link={`/view/${target}`}
-              icon={<FaIdBadge />}
-            />
-          ) : (
-            <p>Scan a badge to get started!</p>
-          )}
+          <p>
+            This is an archived version of the HackTheMidlands 2022 Badge
+            website - if you've still got your badge from the event, you can
+            scan the QR code on it to view your badge, you can no longer edit
+            the code on it.
+          </p>
         </div>
       </div>
     </div>
